9f563bb7dddf7eedfcbb2045b9e41a41e65946b1
Caleb <inchristalway-tech4him@yahoo.com>
2017-08-26 19:58:29 -0600

    Gentle re-authentication modal/popup.

--- src/reducers/auth.js
+++ src/reducers/auth.js
@@ -1,16 +1,18 @@
 import Immutable from 'immutable';
-import { AUTH_REQUEST, AUTH_SUCCESS, AUTH_FAILURE, AUTH_REQUEST_DONE, LOGOUT } from '../actions/auth';
+import { AUTH_REQUEST, AUTH_SUCCESS, AUTH_FAILURE, AUTH_REQUEST_DONE, SHOW_AUTH_POPUP, LOGOUT } from '../actions/auth';
 
-const auth = (state = null, action) => {
+const auth = (state = Immutable.Map(), action) => {
   switch (action.type) {
     case AUTH_REQUEST:
-      return Immutable.Map({ isFetching: true });
+      return state.set('isFetching', true);
     case AUTH_SUCCESS:
       return Immutable.fromJS({ user: action.payload });
     case AUTH_FAILURE:
       return Immutable.Map({ error: action.payload.toString() });
     case AUTH_REQUEST_DONE:
       return state.remove('isFetching');
+    case SHOW_AUTH_POPUP:
+      return state.set('popup', true);
     case LOGOUT:
       return state.remove('user').remove('isFetching');
     default:
